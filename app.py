98255f02640dd5e22c58947c5c882cae8c1c8d9c
Akshat Singh Kushwaha <akshatsingh14372@outlook.com>
2024-08-26 16:40:27 +0530

    chore: Refactor Tic Tac Toe game logic for improved readability and maintainability

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 from tictactoe import check_win, empty_cells, minimax
-import logging
 
 app = Flask(__name__)
-
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
 
 board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 current_player = -1  # Human starts
@@ -14,37 +10,34 @@ computer_wins = 0
 
 @app.route('/')
 def index():
-    global human_wins, computer_wins
-    return render_template('index.html', board=board, current_player=current_player,
+    return render_template('index.html', board=board, current_player=current_player, 
                            human_wins=human_wins, computer_wins=computer_wins)
 
 @app.route('/move/<int:x>/<int:y>', methods=['POST'])
 def move(x, y):
     global board, current_player, human_wins, computer_wins
-    logging.debug(f"Human move at ({x}, {y})")
     if board[x][y] == 0:
         board[x][y] = current_player
         if check_win(current_player, board):
             if current_player == -1:
                 human_wins += 1
-                logging.debug("Human wins!")
-                return render_template('index.html', board=board, current_player=current_player,
-                                       human_wins=human_wins, computer_wins=computer_wins, message="Human wins!")
-            current_player = -current_player
+                message = "Human wins!"
+            else:
+                computer_wins += 1
+                message = "Computer wins!"
+            return render_template('index.html', board=board, current_player=current_player,
+                                   human_wins=human_wins, computer_wins=computer_wins, message=message)
+        current_player = -current_player
 
-        # Computer's move
         if current_player == 1:
-            logging.debug("Computer's turn")
             best_move = minimax(board, current_player, len(empty_cells(board)))
-            logging.debug(f"Computer move at {best_move}")
-            if best_move:
-                board[best_move[0]][best_move[1]] = current_player
-                if check_win(current_player, board):
-                    computer_wins += 1
-                    logging.debug("Computer wins!")
-                    return render_template('index.html', board=board, current_player=current_player,
-                                           human_wins=human_wins, computer_wins=computer_wins, message="Computer wins!")
-                current_player = -current_player
+            board[best_move[0]][best_move[1]] = current_player
+            if check_win(current_player, board):
+                computer_wins += 1
+                message = "Computer wins!"
+                return render_template('index.html', board=board, current_player=current_player,
+                                       human_wins=human_wins, computer_wins=computer_wins, message=message)
+            current_player = -current_player
 
     return render_template('index.html', board=board, current_player=current_player,
                            human_wins=human_wins, computer_wins=computer_wins)
@@ -58,4 +51,4 @@ def reset():
                            human_wins=human_wins, computer_wins=computer_wins)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
--- tictactoe.py
+++ tictactoe.py
@@ -5,55 +5,39 @@ board = [[0,0,0],
          [0,0,0]]
 
 def check_win(player, state):
-    '''
-    this function checks if a player whether Human (-1) or Computer (+1) has won 
-    parameters:
-        player = Computer Or Human
-        state = state of current board
-    win_state:
-        all of the conditions in which a player can win
-    '''
-    win_state = [[state[0][0], state[0][1], state[0][2]],
-                 [state[1][0], state[1][1], state[1][2]],
-                 [state[2][0], state[2][1], state[2][2]],
-                 [state[0][0], state[1][0], state[2][0]],
-                 [state[1][0], state[1][1], state[1][2]],
-                 [state[2][0], state[2][1], state[2][2]],
-                 [state[0][0], state[1][1], state[2][2]],
-                 [state[2][0], state[1][1], state[0][2]]]
-    
-    if [player, player, player] in win_state:
-        return True
-    else:
-        return False
-    
-def empty_cells(state):
-    '''
-    this functions adds empty cells to a set 
-    parameters:
-        state = current state of the board
-    empty_cells:
-        all of the available cells in the board 
-    '''
-    empty_cells = []
-    for x, i in enumerate(state):
-        for y, j in enumerate(i):
-            if j == 0:
-                empty_cells = empty_cells + [[x, y]]
-    return empty_cells
+    """
+    Checks if a player (Human: -1 or Computer: +1) has won.
+    """
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]]
+    ]
 
-def gameover(depth, state):
-    '''
-    checks if the game has finished 
-    parameters
-    '''
-    if depth == 0 or check_win(+1, state) or check_win(-1, state):
-        return True
-    else:
-        return False
+    return [player, player, player] in win_state
+
+def empty_cells(state):
+    """
+    Returns a list of empty cells in the board.
+    """
+    return [[x, y] for x, row in enumerate(state) for y, cell in enumerate(row) if cell == 0]
+
+def gameover(state):
+    """
+    Returns True if the game is over (win or draw).
+    """
+    return check_win(+1, state) or check_win(-1, state) or not empty_cells(state)
 
 def minimax(state, player, depth):
-    if gameover(depth=depth, state=state):
+    """
+    Minimax algorithm for finding the best move for the computer.
+    """
+    if gameover(state) or depth == 0:
         if check_win(+1, state):
             return [-1, -1, +1]
         elif check_win(-1, state):
@@ -62,16 +46,17 @@ def minimax(state, player, depth):
             return [-1, -1, 0]
 
     if player == +1:
-        best = [-1, -1, -float('inf')]
+        best = [-1, -1, -infinity]
     else:
-        best = [-1, -1, +float('inf')]
-    
-    for i in empty_cells(state):
-        state[i[0]][i[1]] = player
+        best = [-1, -1, +infinity]
+
+    for cell in empty_cells(state):
+        x, y = cell
+        state[x][y] = player
         score = minimax(state, -player, depth - 1)
-        state[i[0]][i[1]] = 0
-        score[0], score[1] = i[0], i[1]
-        
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
         if player == +1:
             if score[2] > best[2]:
                 best = score
@@ -80,4 +65,3 @@ def minimax(state, player, depth):
                 best = score
 
     return best
-
